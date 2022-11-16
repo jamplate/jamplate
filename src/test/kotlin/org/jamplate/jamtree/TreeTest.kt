fdfdde3216dcc1b434bdd8bd045fc33354fd7721
Sulaiman Oboody <lsafer@cufy.org>
2022-11-16 20:32:24 +0300

    test: Updated tests

--- src/test/kotlin/org/jamplate/jamtree/TreeTest.kt
+++ src/test/kotlin/org/jamplate/jamtree/TreeTest.kt
@@ -13,10 +13,9 @@
  *	See the License for the specific language governing permissions and
  *	limitations under the License.
  */
-package org.jamplate.test.org.jamplate.jamtree
+package org.jamplate.jamtree
 
 import org.jamplate.jamcore.*
-import org.jamplate.jamtree.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Test
